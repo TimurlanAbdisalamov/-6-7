a97c2fb6daad9af186ca8731f7f8d2e119362c60
TimurlanAbdisalamov <timurlanabdisalamov@gmail.com>
2024-12-19 17:07:46 +0600

    первый коммит

--- src/components/CategoryComponent/CategoryComponent.jsx
+++ src/components/CategoryComponent/CategoryComponent.jsx
@@ -3,29 +3,40 @@ import axios from "axios";
 import Card from '../Card/Card';
 import { Link } from "react-router-dom";
 
-const CategoryComponent = ({limit, category}) => {
-    const [data, setData] = useState([]);
+const CategoryComponent = ({ limit, category }) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true); 
+  useEffect(() => {
+    setLoading(true); 
+    setData([]); 
+    axios(
+      limit
+        ? `https://fakestoreapi.com/products/category/${category}?limit=${limit}`
+        : `https://fakestoreapi.com/products/category/${category}`
+    )
+      .then(({ data }) => {
+        setData(data); 
+        setLoading(false); 
+      })
+      .catch(() => {
+        setLoading(false); 
+      });
+  }, [category, limit]);
 
-    useEffect(()=>{
-        setData([])
-        axios(
-            limit
-            ? `https://fakestoreapi.com/products/category/${category}?limit=${limit}`
-            : `https://fakestoreapi.com/products/category/${category}`
-        ).then(({data})=> setData(data))
-    }, [category, limit])
-    return (
-        <div className="container">
-            <h1><Link to={`/category/${category}`}>{category}</Link></h1>
+  return (
+    <div className="container">
+      <h1><Link to={`/category/${category}`}>{category}</Link></h1>
+      {loading ? (
+        <div className="loader"></div> 
+      ) : (
         <div className="row">
-        {
-            data.map(item => {
-                return <Card item={item} key={item.id} />
-            })
-        }
+          {data.map(item => {
+            return <Card item={item} key={item.id} />;
+          })}
         </div>
-        </div>
-    );
-}
+      )}
+    </div>
+  );
+};
 
 export default CategoryComponent;
