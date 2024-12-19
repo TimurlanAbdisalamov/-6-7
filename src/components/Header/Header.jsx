e1d3b049b5828f6ad0ba86aead4313789000acec
TimurlanAbdisalamov <timurlanabdisalamov@gmail.com>
2024-12-20 00:50:11 +0600

    ldmskdosdimc

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,30 +3,38 @@ import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 const Header = () => {
-    const categories = useSelector(s => s.reducer.categories);
+    const categories = useSelector((state) => state.reducer.categories);
+    const cart = useSelector((state) => state.reducer.cart);
+
+    const totalItems = cart.reduce((acc, item) => acc + item.count, 0);
+
     return (
         <header className='header'>
             <div className="header-container container">
-
-
-            <h1 className='header-logo'><Link to={'/'} >shop</Link></h1>
-
-            <nav className="header-nav">
-                {
-                    categories.map(item =>{
-                        return <Link
-                        key={item}
-                        to={`/category/${item}`}
-                        className='header-nav-link'
-                        >{item}</Link>
-                    })
-                }
-                <Link to={'/'} className='header-nav-link'>home</Link>
-                <Link to={'/cart'} className='header-nav-link'>cart</Link>
-            </nav>
+                <h1 className='header-logo'><Link to={'/'}>shop</Link></h1>
+                <nav className="header-nav">
+                    {categories.map((item) => (
+                        <Link
+                            key={item}
+                            to={`/category/${item}`}
+                            className='header-nav-link'
+                        >
+                            {item}
+                        </Link>
+                    ))}
+                    <Link to={'/'} className='header-nav-link'>home</Link>
+                    <div className="header-cart">
+                        <Link to={'/cart'} className='header-nav-link'>
+                            cart
+                        </Link>
+                        {totalItems > 0 && (
+                            <span className="header-cart-badge">{totalItems}</span>
+                        )}
+                    </div>
+                </nav>
             </div>
         </header>
     );
-}
+};
 
 export default Header;
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -6,12 +6,12 @@ const ADD_CART = 'ADD_CART';
 const DECREMENT_CART = 'DECREMENT_CART';
 const DELETE_CART = 'DELETE_CART';
 
-export const getCategories = () =>{
-    let action = createAction(GET_CATEGORIES);
-    return (dispatch) =>{
-        axios('https://fakestoreapi.com/products/categories')
-        .then(({data}) => dispatch(action(data)))
-    }
+export const getCategories = () => {
+  let action = createAction(GET_CATEGORIES);
+  return (dispatch) => {
+    axios('https://fakestoreapi.com/products/categories')
+      .then(({ data }) => dispatch(action(data)))
+  }
 };
 
 export const addCart = createAction(ADD_CART);
@@ -19,36 +19,44 @@ export const decrementCart = createAction(DECREMENT_CART);
 export const deleteCart = createAction(DELETE_CART);
 
 const initialState = {
-    categories: [],
-    cart: [],
-}
+  categories: [],
+  cart: JSON.parse(localStorage.getItem('cart')) || [],
+};
 
+const saveCartToLocalStorage = (cart) => {
+  localStorage.setItem('cart', JSON.stringify(cart));
+};
 
-export default createReducer(initialState, (builder)=>{
-    builder
-    .addCase(GET_CATEGORIES, (state, action)=>{
-        state.categories = action.payload
+export default createReducer(initialState, (builder) => {
+  builder
+    .addCase(GET_CATEGORIES, (state, action) => {
+      state.categories = action.payload;
     })
-    .addCase(ADD_CART, (state, action)=>{
-        let object = action.payload
-        const idx = state.cart.findIndex(item => object.id === item.id);
-        if(idx < 0){
-            state.cart = [{
-                ...object,
-                count: 1
-            }, ...state.cart]
-        } else{
-            state.cart[idx].count++
-            state.cart = [...state.cart]
-        }
+    .addCase(ADD_CART, (state, action) => {
+      let object = action.payload;
+      const idx = state.cart.findIndex(item => object.id === item.id);
+      if (idx < 0) {
+        state.cart = [{
+          ...object,
+          count: 1
+        }, ...state.cart];
+      } else {
+        state.cart[idx].count++;
+        state.cart = [...state.cart];
+      }
+      saveCartToLocalStorage(state.cart); 
     })
-    .addCase(DECREMENT_CART, (state, action)=>{
-        let object = action.payload
-        const idx = state.cart.findIndex(item => object.id === item.id);
-        state.cart[idx].count--
-        state.cart = [...state.cart]
+    .addCase(DECREMENT_CART, (state, action) => {
+      let object = action.payload;
+      const idx = state.cart.findIndex(item => object.id === item.id);
+      if (state.cart[idx].count > 1) {
+        state.cart[idx].count--;
+        state.cart = [...state.cart];
+      }
+      saveCartToLocalStorage(state.cart); 
     })
-    .addCase(DELETE_CART, (state, action)=>{
-       state.cart = state.cart.filter(item => item.id !== action.payload.id)
-    })
+    .addCase(DELETE_CART, (state, action) => {
+      state.cart = state.cart.filter(item => item.id !== action.payload.id);
+      saveCartToLocalStorage(state.cart); 
+    });
 });
